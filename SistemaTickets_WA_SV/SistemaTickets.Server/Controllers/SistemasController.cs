73f983fad3c14989de5426fa4d73de7e0f194325
Aaron Hernandez <126830445+aaron-developer25@users.noreply.github.com>
2024-02-25 15:39:22 -0400

    Hemos generado nuestro tercer controlador (SistemasControllers)

---
+++ SistemaTickets_WA_SV/SistemaTickets.Server/Controllers/SistemasController.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Biblioteca.Models;
+using SistemaTickets.Server.DAL;
+
+namespace SistemaTickets.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SistemasController : ControllerBase
+    {
+        private readonly Contexto _context;
+
+        public SistemasController(Contexto context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Sistemas
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Sistemas>>> GetSistemas()
+        {
+            return await _context.Sistemas.ToListAsync();
+        }
+
+        // GET: api/Sistemas/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Sistemas>> GetSistemas(int id)
+        {
+            var sistemas = await _context.Sistemas.FindAsync(id);
+
+            if (sistemas == null)
+            {
+                return NotFound();
+            }
+
+            return sistemas;
+        }
+
+        // PUT: api/Sistemas/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut]
+        public async Task<ActionResult<Sistemas>> PutSistemas(Sistemas sistemas)
+        {
+            if (SistemasExists(sistemas.SistemaId))
+            {
+                _context.Sistemas.Update(sistemas);
+            }
+            await _context.SaveChangesAsync();
+            return Ok(sistemas);
+        }
+
+        // POST: api/Sistemas
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Sistemas>> PostSistemas(Sistemas sistemas)
+        {
+            _context.Sistemas.Add(sistemas);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetSistemas", new { id = sistemas.SistemaId }, sistemas);
+        }
+
+        // DELETE: api/Sistemas/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteSistemas(int id)
+        {
+            var sistemas = await _context.Sistemas.FindAsync(id);
+            if (sistemas == null)
+            {
+                return NotFound();
+            }
+
+            _context.Sistemas.Remove(sistemas);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool SistemasExists(int id)
+        {
+            return _context.Sistemas.Any(e => e.SistemaId == id);
+        }
+    }
+}
